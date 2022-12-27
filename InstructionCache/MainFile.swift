df3dcfab99aa05d1b14c10c1868ce5520340b2d1
Philip Turner <philipturner.AR@gmail.com>
2022-12-26 23:17:07 -0500

    Update MainFile.swift

--- InstructionCache/MainFile.swift
+++ InstructionCache/MainFile.swift
@@ -18,6 +18,9 @@ func mainFunc() {
   let numTrials = 3
   let theoreticalGOPS = 5308 // 855
   
+  // A14 @ 1270 MHz
+  // A15 @ 1335 MHz
+  
   // Specify amount of work done.
   let numInstructions = 720 * 2 + 120 * 0 + 24 * 0 // 120 * 2
   
@@ -25,24 +28,25 @@ func mainFunc() {
   if is32Bit {
     // FADD32 configs
     configs = [
-//      1 : (30, 31 * 1024), // 11 (30 simds)
+      1 : (32, 32 * 1024), // 11 (30 simds)
       2 : (23, 31 * 1024), // 19 (46 simds)
       3 : (15, 20 * 1024), // 28 (45 simds)
       4 : (11, 16 * 1024), // 36 (44 simds)
       5 : ( 9, 12 * 1024), // 41 (45 simds)
       6 : ( 8, 12 * 1024), // 44 (48 simds)
-      7 : ( 6,  8 * 1024), // 53 (42 simds)
+//      7 : ( 6,  8 * 1024), // 53 (42 simds)
       8 : ( 4,  8 * 1024), // 62 (32 simds)
     ]
   } else {
     // FADD16 configs
     configs = [
+//      2 : (32, 32 * 1024), // 19 (62 simds)
       2 : (31, 31 * 1024), // 22 (62 simds)
       3 : (27, 20 * 1024), // 31 (81 simds)
       4 : (20, 16 * 1024), // 42 (80 simds)
       5 : (16, 10 * 1024), // 53 (80 simds)
       6 : ( 8, 10 * 1024), // 64 (48 simds)
-      7 : (10,  8 * 1024), // 67 (70 simds)
+//      7 : (10,  8 * 1024), // 67 (70 simds)
       8 : ( 8,  8 * 1024), // 76 (64 simds)
     ]
   }
@@ -55,16 +59,23 @@ func mainFunc() {
   let library = device.makeDefaultLibrary()!
   let function = library.makeFunction(name: "testCache")!
   let pipeline = try! device.makeComputePipelineState(function: function)
-  precondition(pipeline.maxTotalThreadsPerThreadgroup == 1024)
+  let maxSimdsPerThreadgroup = pipeline.maxTotalThreadsPerThreadgroup / 32
+  if maxSimdsPerThreadgroup < 32 {
+    print("Overallocated registers: \(maxSimdsPerThreadgroup) simds")
+  }
   
   let inputsBuffer = device.makeBuffer(length: 1024 * 1024)!
   let outputsBuffer = device.makeBuffer(length: 1024 * 1024)!
   
   // Sampled measurements in GFLOPS.
-  var allSamples: [Int: Int] = [:]
+  var allSamples: [Int: Double] = [:]
   for _ in 0..<numTrials {
     for residentThreadgroups in residentThreadgroupsRange {
-      let simdsPerThreadgroup = configs[residentThreadgroups]!.simdsPerThreadgroup
+      guard let config = configs[residentThreadgroups] else {
+        continue
+      }
+      let simdsPerThreadgroup = min(
+        maxSimdsPerThreadgroup, config.simdsPerThreadgroup)
       let maxActiveSimdgroupsRange: [Int] = (1...simdsPerThreadgroup).map { $0 }
       let numThreadgroups = numCores * residentThreadgroups * overSubscription
       
@@ -83,7 +94,7 @@ func mainFunc() {
           let numOPs = numThreads * numInstructions
           
           let numGOPS = Double(numOPs) / time / 1e9
-          let opsPerCycle = Int(128 * numGOPS / Double(theoreticalGOPS))
+          let opsPerCycle = Double(theoreticalGOPS) / numGOPS
           let occupancy = residentThreadgroups * activeSimdgroups
           
           if let previousSample = allSamples[occupancy] {
@@ -118,9 +129,9 @@ func mainFunc() {
   // Display each trial's results, then the maximum.
   print("Occupancy Benchmark")
   let allowedKeys = [
-    2, 3, 4, 5, 6, 7, 8, 10,
-    12, 14, 15, 16, 18, 20,
-    21, 24, 28, 30, 32
+    1,2, 3, 4, 5, 6, 8, 10,
+    12, 15, 16, 18, 20,
+    24, 30, 32, 36, 40, 48, 56, 64, 72
   ]
   for key in allSamples.keys.sorted() where allowedKeys.contains(key) {
     // TODO: Blacklist certain highly prime numbers.
@@ -128,7 +139,8 @@ func mainFunc() {
     var line = String()
     line.append(alignInteger(key, length: 2))
     line.append(" -> ")
-    line.append(alignInteger(sample, length: 3))
+    line.append(String(format: "%.2f", sample))
+//    line.append(alignInteger(sample, length: 4))
     print(line)
   }
 //  for i in maxActiveSimdgroupsRange.indices {
